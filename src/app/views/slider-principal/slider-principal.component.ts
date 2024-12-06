1301e6a7f3164e85b3f167e1b59f4e16da1a9d63
JoseRazo <joserrp.13@gmail.com>
2024-12-06 10:54:47 -0600

    publicacion de informacion contraloria social 2024

--- src/app/views/slider-principal/slider-principal.component.ts
+++ src/app/views/slider-principal/slider-principal.component.ts
@@ -18,4 +18,10 @@ export class SliderPrincipalComponent implements OnInit {
     window.open('assets/content/seguro-facultativo/AVISO_DE_ACCIDENTE_PREVEM_(002).pdf', '_blank');
   }
 
+  openDocumentsContraloria(event: Event): void {
+    event.preventDefault();
+    window.open('assets/content/contraloria-social/Convocatoria-CS-U006-2024.pdf', '_blank');
+    window.open('assets/content/contraloria-social/TRIPTICO-CS-U006-24.pdf', '_blank');
+  }
+
 }
